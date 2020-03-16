5eb813259bf994c334a4359a20534f3cd2c494a6
elcarvalho <lessa.eric@gmail.com>
2020-03-16 20:38:36 -0300

    chore(estrutura): buscando produtos da api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import api from '../../services/api';
+
+import { formatPrice } from '../../util/format';
 
 import { MdAddShoppingCart } from 'react-icons/md';
 
 import { ProductList } from './styles';
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await api.get('/products');
+
+      const products = response.data.map(product => ({
+        ...product,
+        priceFormatted: formatPrice(product.price),
+      }));
+
+      setProducts(products);
+    };
+
+    getProducts();
+  }, []);
+
   return (
     <ProductList>
-      <li>
-        <img src="https://dummyimage.com/250x250/4e4e4e/fff" alt="Tênis" />
-        <strong>Tênis</strong>
-        <span>R$ 129,90</span>
+      {products.map(product => (
+        <li key={String(product.id)}>
+          <img src={product.image} alt={product.title} />
+          <strong>{product.title}</strong>
+          <span>{product.priceFormatted}</span>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#fff" /> 3
+            </div>
+            <span>Adicionar ao carrinho</span>
+          </button>
+        </li>
+      ))}
     </ProductList>
   );
 }
