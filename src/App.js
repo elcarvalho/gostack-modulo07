9956300a3e7a6b6c1a436b74a96084017178d8cc
elcarvalho <lessa.eric@gmail.com>
2020-03-16 19:08:14 -0300

    chore(estrutura): estilização da home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import Globals from './styles/globals';
+import Header from './components/Header';
+
 import Routes from './routes';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes />
+      <Globals />
     </BrowserRouter>
   );
 }
